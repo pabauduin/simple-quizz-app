23b21c8c12bbf7f21aeabe1d47c08bdbe5342ea1
Pierre-Antoine Bauduin <pab@macbook-air-aria-de-pab.home>
2023-06-13 12:41:55 +0200

    add user score

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -14,17 +14,32 @@ class QuestionsController < ApplicationController
 
   def validate
     @question = Question.find(params[:id])
+    user = User.find(params[:user_id])
     if @question.answer.downcase == params[:validate][:user_answer].downcase
       @question.success = true
       @question.save
+      user.score = Question.all.where(user_id: params[:user_id]).where(success: true).count
+      user.score = "max" if define_number_of_questions.to_s == user.score
+      user.save
+      puts "---"
+      puts "user.score"
+      puts "---"
     end
-    redirect_to user_question_path(user_id: params[:user_id], id: define_first_answered_question)
+    if user.score != "max"
+      redirect_to user_question_path(user_id: params[:user_id], id: define_first_answered_question)
+    else
+      redirect_to root_path
+    end
   end
 
-  def define_question_number
-    number_of_questions = Question.where(user_id: params[:user_id]).count()
+
+  def define_number_of_questions
+    Question.all.where(user_id: params[:user_id]).count()
+  end
+
+  def define_question_number 
     remaining_questions = Question.where(user_id: params[:user_id]).where(success: false).count()
-    (number_of_questions - remaining_questions) + 1
+    (define_number_of_questions  - remaining_questions) + 1
   end
 
   def define_first_answered_question
